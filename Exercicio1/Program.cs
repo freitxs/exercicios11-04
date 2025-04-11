00a7f2e0971c583bf12cd31951aa77db3b5f8f73
Lucas Freitas <89039175+freitxs@users.noreply.github.com>
2025-04-11 20:29:29 -0300

    Add files via upload

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+        Console.WriteLine($"Olá, {nome}! Seja bem-vindo(a)!");
+    }
+}
